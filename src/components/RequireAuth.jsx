d899c7324d8c6e335e87f99fda9c50c28c3d55d5
rexes123 <rexes_123@hotmail.com>
2024-10-18 15:17:46 +0800

    update

---
+++ src/components/RequireAuth.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../AuthContext";
+
+export default function RequireAuth({ children }){
+    const token = useContext(AuthContext).token;
+    console.log(token);
+
+    if (!token){
+        return <Navigate to="/login" replace/>
+    }
+
+    return children;
+}
